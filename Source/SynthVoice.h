e40a659ff62bb02423714a18e0f54e1594f5811e
8bitPandaPlugins <45884278+8bitPandaPlugins@users.noreply.github.com>
2019-04-13 18:19:09 +0100

    Revert "ITS STILL BROKEN WHYYYYYYYYYYYYYYYY"
    This reverts commit ea9dc9689fac0f34b6f56d5cfb2d40cf894510fc.

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -25,13 +25,13 @@ struct SynthSound : public SynthesiserSound
 };
 class Voice: public SynthesiserVoice {
 public:
-	Voice(int sampleRate = 44100 ):Filter(10000,1,sampleRate,1),Buffer(sampleRate*5)
+	Voice(int sampleRate = 44100):Filter(10000,1,sampleRate,1),Buffer(sampleRate*5)
 	{
 		ADSR::Parameters peram;
-		peram.attack = 5;
-		peram.decay = 3;
-		peram.sustain = 3;
-		peram.release = 2;
+		peram.attack = 0;
+		peram.decay = 50;
+		peram.sustain = 10;
+		peram.release = 5;
 		Envelope.setParameters(peram);
 
 	}
@@ -40,7 +40,7 @@ public:
 
 	bool canPlaySound(SynthesiserSound* sound) override
 	{
-		return dynamic_cast<SynthSound*> (sound) != nullptr;
+		return dynamic_cast<SynthesiserSound*> (sound) != nullptr;
 	}
 
 	void startNote(int midiNoteNumber, float velocity,
@@ -50,18 +50,12 @@ public:
 		auto CyclesPerSample = CyclesPerSecond / getSampleRate();
 		Envelope.noteOn();	
 		noiseburst = true;
-		
 	}
-	
-	void stopNote(float /*velocity*/, bool allowTailOff = true) override {
-		if (allowTailOff)
-		{
-			if (tailOff < 0.005)
-				tailOff = 1.0;
-		}
-		
+
+	void stopNote(float /*velocity*/, bool allowTailOff) override {
+		Envelope.noteOff();
 		noiseburst = false;
-		tailOff -= 0.005;
+		//fire = 0;
 	}
 
 	void pitchWheelMoved(int) override {
@@ -78,41 +72,28 @@ public:
 		float filtered, out;
 		float * left = outputBuffer.getWritePointer(0);
 		float * right = outputBuffer.getWritePointer(1);
-		
+		for (int i = 0; i < outputBuffer.getNumSamples(); i++)
+		{
 
-			for (int i = 0; i < outputBuffer.getNumSamples(); i++)
-			{
-
-				if ((noiseburst) && (fire < (5000 * 44.1))) {
-				
-				out =  rand.nextFloat() * 0.25f - 0.125f;
-					//out = noise::MakeWNoise();
-					//Envelope.noteOff();
-					
-				}
-				else {
-				out = Buffer.getDelay(500);
-				filtered = Filter.process_samples(out);
-				Buffer.writeDelay(filtered * 0.97);
-				}
-				left[i] += out;
-				right[i] += out;
-
-				if (!Envelope.isActive())
-					clearCurrentNote();
-
-				fire++;
-				tailOff *= 0.99;
-
-				if (tailOff < 0.05) {
-					Envelope.noteOff();
-					clearCurrentNote();
-				}
+			if (noiseburst == true && fire < 50 * 44.1) {
+				out = noise::MakeWNoise();
 			}
-			Envelope.applyEnvelopeToBuffer(outputBuffer, startsample, numsamples);
-			
 
+			out = Buffer.getDelay(200);
+			filtered += Filter.process_samples(out);
+			Buffer.writeDelay(out * 0.97);
+			
+			left[i] = out;
+			right[i] = out;
+
+
+			fire++;
+
+		}
+		Envelope.applyEnvelopeToBuffer(outputBuffer, startsample, numsamples);
+;
 	}
+	
 
 private:
 	LowPass Filter;
@@ -120,10 +101,8 @@ private:
 	DelayUnit Buffer;
 	ADSR Envelope;
 	bool noiseburst;
-	int fire = 0;
-	int offcount;
-	float tailOff = 1.0;
-	Random rand;
+	int fire;
+
 };
 class SynthAudioSource : public AudioSource
 {
@@ -151,7 +130,7 @@ public:
 		
 		//midiCollector.removeNextBlockOfMessages(incomingMidi, bufferToFill.numSamples);
 		
-		//m_Keystate.processNextMidiBuffer(incomingMidi, bufferToFill.startSample, bufferToFill.numSamples, true);
+		m_Keystate.processNextMidiBuffer(incomingMidi, bufferToFill.startSample, bufferToFill.numSamples, true);
 
 		m_Synth.renderNextBlock(*bufferToFill.buffer, incomingMidi, bufferToFill.startSample, bufferToFill.numSamples);
 
